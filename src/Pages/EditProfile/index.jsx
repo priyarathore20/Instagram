03915ae630235a84a1f24194bf8ea64a1a6b58ee
priya rathore <rathorepriya1705@gmail.com>
2023-07-28 00:57:25 +0530

    next commit

--- src/Pages/EditProfile/index.jsx
+++ src/Pages/EditProfile/index.jsx
@@ -1,65 +1,44 @@
-import React from 'react';
-// import './styles.css';
-import {
-  Typography,
-  AppBar,
-  Toolbar,
-  TextField,
-  Button,
-  Box,
-  MenuItem,
-} from '@mui/material';
+import React, { useState } from 'react';
+import './styles.css';
+import Input from '../../Components/Input';
+import Logo from '../../Components/Logo';
 
 const EditPage = () => {
+  const [username, setUsername] = useState('')
+  const [fullName, setFullName] = useState('')
+const [bio, setBio] = useState('')
+const [email, setEmail] = useState('')
+const [gender, setGender ] = useState('')
+
   return (
-    <div className="App">
-      
-      <form>
-        <TextField
-          style={{ width: '200px', margin: '5px' }}
-          type="text"
-          label="Set new username"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ width: '200px', margin: '5px' }}
-          type="text"
-          label="Enter full name"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ width: '200px', margin: '5px' }}
-          type="text"
-          label="Enter bio"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ width: '200px', margin: '5px' }}
-          type="text"
-          label="Gender"
-          variant="outlined"
-        />
-        <br />
-        <TextField
-          style={{ width: '200px', margin: '5px' }}
-          type="text"
-          label="Update email"
-          variant="outlined"
-        />
-        <br />
-        <input 
-        type='file'
-        style={{ width: '200px', margin: '5px' }}
-        aria-label='Upload image'
-        />
-        <br />
-        <Button variant="contained" color="primary">
-          save
-        </Button>
-      </form>
+    <div className="edit">
+      <div className="app">
+        <form className="edit-form">
+          <div>
+            <Logo />
+          </div>
+          <h2>Edit your profile :</h2>
+          <div>
+            <Input placeholder="Set username" type="text" onChange={e => setUsername(e.target.value)} value={username}/>
+            <br />
+            <Input placeholder="Enter full name" type="text" onChange={e => setFullName(e.target.value)} value={fullName} />
+            <br />
+            <Input placeholder="Enter your bio..." type="text" onChange={e => setBio(e.target.value)} value={bio}/>
+            <br />
+            <Input placeholder="Gender" type="text" onChange={e => setGender(e.target.value)} value={gender}/>
+            <br />
+            <Input placeholder="Update email" type="text" onChange={e => setEmail(e.target.value)} value={email}/>
+            <br />
+            <input
+              className="upload-img"
+              type="file"
+              placeholder="Upload image"
+            />
+            <br />
+            <button type="submit" className='edit-btn'>Save</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
