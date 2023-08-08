afe9406328305c36466296ee9ca1199f6cf6ba3d
priya rathore <rathorepriya1705@gmail.com>
2023-08-08 11:59:35 +0530

    next commit

--- src/Pages/SearchPage/index.jsx
+++ src/Pages/SearchPage/index.jsx
@@ -1,32 +1,27 @@
 import { Avatar } from '@mui/material';
 import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-import { getFirestore, collection } from 'firebase/firestore';
+import { getFirestore, collection, where, getDocs } from 'firebase/firestore';
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
   const [fetchedUsers, setFetchedUsers] = useState('');
   const db = getFirestore();
 
-  const initial = fetchedUsers.charAt[0];
-  const result = db
-    .collection('Profiles')
-    .where('name', '==', query)
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        console.log(doc.id, ' => ', doc.data());
-      });
-    })
-    .catch((error) => {
-      console.log('Error getting documents: ', error);
+  const searchUsers = async (e) => {
+    e.preventDefault();  
+    const q = query(collection(db, 'Profiles'), where('name', '==', query));
+    const querySnapshot = await getDocs(q);    
+    const users = [];
+    querySnapshot.forEach((doc) => {
+      users.push(doc.data());
     });
 
-  setFetchedUsers(result);
-
+    setFetchedUsers(users);
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={searchUsers}>
         <input
           type="text"
           placeholder="Search here..."
@@ -37,18 +32,22 @@ const SearchPage = () => {
           <FaSearch />
         </button>
       </form>
+      <>
       <div>
-        <div className="search-card">
-          <div>
-            <Avatar>{initial}</Avatar>
+        {fetchedUsers.map((user, index) => (
+          <div className="search-card" key={index}>
+            <div>
+              <Avatar>{user.name.charAt(0)}</Avatar>
+            </div>
+            <div>
+              <p>{user.username}</p>
+              <p>{user.name}</p>
+            </div>
           </div>
-          <div>
-            <p>{fetchedUsers.username}</p>
-            <p>{fetchedUsers.name}</p>
-          </div>
-        </div>
+        ))}
       </div>
-    </div>
+        </>
+      </div>
   );
 };
 
