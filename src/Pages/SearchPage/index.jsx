cffb181e61aa9f5a2743cd4e81fafc01185e70b5
priya rathore <rathorepriya1705@gmail.com>
2023-08-08 11:31:36 +0530

    next commit

--- src/Pages/SearchPage/index.jsx
+++ src/Pages/SearchPage/index.jsx
@@ -1,16 +1,28 @@
 import { Avatar } from '@mui/material';
 import React, { useState } from 'react';
-import {FaSearch} from 'react-icons/fa'
-import {getFirestore, collection } from 'firebase/firestore'
+import { FaSearch } from 'react-icons/fa';
+import { getFirestore, collection } from 'firebase/firestore';
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
-  const [fetchedUsers, setFetchedUsers] = useState('')
-  const db = getFirestore()
+  const [fetchedUsers, setFetchedUsers] = useState('');
+  const db = getFirestore();
 
-const initial = fetchedUsers.charAt[0]
-const result = db.collection('Profiles').where('name', '==', query ).get()
-setFetchedUsers(result)
+  const initial = fetchedUsers.charAt[0];
+  const result = db
+    .collection('Profiles')
+    .where('name', '==', query)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data());
+      });
+    })
+    .catch((error) => {
+      console.log('Error getting documents: ', error);
+    });
+
+  setFetchedUsers(result);
 
   return (
     <div>
@@ -27,7 +39,9 @@ setFetchedUsers(result)
       </form>
       <div>
         <div className="search-card">
-          <div><Avatar>{initial}</Avatar></div>
+          <div>
+            <Avatar>{initial}</Avatar>
+          </div>
           <div>
             <p>{fetchedUsers.username}</p>
             <p>{fetchedUsers.name}</p>
