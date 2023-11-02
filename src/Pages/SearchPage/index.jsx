5b33ec04654460fa34d5fd8a7d1e49a9a95fc0a9
Priya Rathore <rathorepriya1705@gmail.com>
2023-11-02 18:01:52 +0530

    responsiveness

--- src/Pages/SearchPage/index.jsx
+++ src/Pages/SearchPage/index.jsx
@@ -1,5 +1,5 @@
-import { Avatar } from "@mui/material";
-import React, { useState } from "react";
+import { Avatar, debounce } from "@mui/material";
+import React, { useCallback, useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import {
   getFirestore,
@@ -15,39 +15,81 @@ import { useSnackbar } from "notistack";
 const SearchPage = () => {
   const [queryText, setQueryText] = useState("");
   const [fetchedUsers, setFetchedUsers] = useState([]);
+  const [searchLoading, setSearchLoading] = useState(false);
   const db = getFirestore();
   const { enqueueSnackbar } = useSnackbar();
 
-  const searchUsers = async (e) => {
-    e.preventDefault();
+  // const searchUsers = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const q = query(
+  //       collection(db, "Profiles"),
+  //       where("name", "==", queryText)
+  //     );
+  //     const querySnapshot = await getDocs(q);
+  //     const users = [];
+  //     querySnapshot.forEach((doc) => {
+  //       users.push(doc.data());
+  //     });
+  //     setFetchedUsers(users);
+  //   } catch (error) {
+  //     enqueueSnackbar(error, { variant: "error" });
+  //   }
+  // };
+
+  const searchUser = async (valueToMatch) => {
     try {
-      const q = query(
-        collection(db, "Profiles"),
-        where("name", "==", queryText)
-      );
+      setSearchLoading(true);
+      if (valueToMatch === undefined) {
+        // Handle the case where valueToMatch is undefined, e.g., set a default value or return early.
+        setSearchLoading(false);
+        return;
+      }
+      const collectionRef = collection(db, "Profiles");
+      const fieldToQuery = "name";
+      const q = query(collectionRef, where(fieldToQuery, "==", valueToMatch));
       const querySnapshot = await getDocs(q);
-      const users = [];
+      // console.log(querySnapshot);
       querySnapshot.forEach((doc) => {
-        users.push(doc.data());
+        console.log(doc.data());
+        setFetchedUsers(doc.data());
       });
-      setFetchedUsers(users);
+      setSearchLoading(false);
     } catch (error) {
+      setSearchLoading(false);
       enqueueSnackbar(error, { variant: "error" });
+      console.log(error);
     }
   };
+
+  const debounceForSearch = debounce(searchUser, 500);
+  const optimizedFn = useCallback(debounceForSearch, []);
+
+  const handleSearchInputChange = (e) => {
+    e.preventDefault();
+    let value = e.target.value;
+    setQueryText(value);
+    // setFetchedUsers(null);
+    optimizedFn(value);
+  };
+
   return (
     <div className="search-box">
       <div className="search-body">
         <Logo />
-        <form onSubmit={searchUsers}>
+        <form onSubmit={searchUser}>
           <input
             className="user-search-input"
             type="text"
             placeholder="Search here..."
             value={queryText}
-            onChange={(e) => setQueryText(e.target.value)}
+            onChange={handleSearchInputChange}
           />
-          <button className="explore-btn" type="submit">
+          <button
+            className="explore-btn"
+            type="submit"
+            disabled={searchLoading}
+          >
             <FaSearch />
           </button>
         </form>
--- src/Router/routes.jsx
+++ src/Router/routes.jsx
@@ -1,55 +1,59 @@
-import Homepage from '../Pages/Home';
-import ProfilePage from '../Pages/Profile';
-import EditPage from '../Pages/EditProfile';
-import ExplorePage from '../Pages/ExplorePage';
-import LoginPage from '../Pages/Login';
-import Signup from '../Pages/Signup';
-import NotFound from '../Pages/404';
-import SearchPage from '../Pages/SearchPage';
+import Homepage from "../Pages/Home";
+import ProfilePage from "../Pages/Profile";
+import EditPage from "../Pages/EditProfile";
+import ExplorePage from "../Pages/ExplorePage";
+import LoginPage from "../Pages/Login";
+import Signup from "../Pages/Signup";
+import NotFound from "../Pages/404";
+import SearchPage from "../Pages/SearchPage";
 
 export const routes = [
   {
-    name: '404',
-    path: '*',
+    name: "404",
+    path: "*",
     component: NotFound,
     isProtected: false,
   },
   {
-    name: 'landing page',
-    path: '/',
+    name: "landing page",
+    path: "/",
     component: LoginPage,
     isProtected: false,
   },
   {
-    name: 'Edit Profile',
-    path: '/edit-profile',
+    name: "Edit Profile",
+    path: "/edit-profile",
     component: EditPage,
     isProtected: false,
   },
   {
-    name: 'home',
-    path: '/home',
+    name: "home",
+    path: "/home",
     component: Homepage,
-    isProtected: true,
-  },{
-    name: 'Profile',
-    path: '/profile',
+    isProtected: false,
+  },
+  {
+    name: "Profile",
+    path: "/profile",
     component: ProfilePage,
     isProtected: false,
-  },{
-    name: 'Explore',
-    path: '/explore',
+  },
+  {
+    name: "Explore",
+    path: "/explore",
     component: ExplorePage,
-    isProtected: true,
-  },{
-    name: 'Search',
-    path: '/search',
+    isProtected: false,
+  },
+  {
+    name: "Search",
+    path: "/search",
     component: SearchPage,
-    isProtected: true,
-  },{
-    name: 'signup',
-    path: '/signup',
+    isProtected: false,
+  },
+  {
+    name: "signup",
+    path: "/signup",
     component: Signup,
     isProtected: false,
   },
-]
+];
